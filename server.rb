bdeac2e24143ddeae8c135de5d5df23e0172f08a
Christian Aracena <CDAracena@gmail.com>
2018-05-21 17:41:49 -0400

    Adde mailgun functionality ruby

--- server.rb
+++ server.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
 require 'sendgrid-ruby'
+require 'mailgun'
 
 
 class Cookie
@@ -34,26 +35,21 @@ get('/') do
   erb(:index)
 end
 
-# post('/') do
-#   # include SendGrid
-#
-#   from = Email.new(email: 'test@example.com')
-#   to = Email.new(email: 'test@example.com')
-#   subject = 'Sending with SendGrid is Fun'
-#   content = Content.new(type: 'text/plain', value: 'and easy to do anywhere, even with Ruby')
-#   mail = Mail.new(from, subject, to, content)
-#
-#   sg = SendGrid::API.new(api_key: ENV['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
-# '])
-#   response = sg.client.mail._('send').post(request_body: mail.to_json)
-#   puts response.status_code
-#   puts response.body
-#   puts response.parsed_body
-#   puts response.headers
-#
-#   redirect '/'
-#   erb(:layout)
-# end
+post('/') do
+  mg_client = Mailgun::Client.new(ENV['MAIL_GUN_API_KEY'])
+
+# Define your message parameters
+message_params =  { from: 'crebelz174@gmail.com',
+                  to:   params[:inputParams],
+                  subject: 'Quisqueya Sweets Newsletter!',
+                  text:    'Thank you, you are on your first path to embracing <h1>Dominican sweetness!</h1>'
+                }
+
+# Send your message through the client
+mg_client.send_message(ENV['MAIL_GUN_API_DOMAIN'], message_params)
+
+  redirect '/'
+end
 
 get('/cookies') do
   first_cookie = Cookie.new('Supa Chocolate Chip', "$100", "chocolatechip1.jpg")
